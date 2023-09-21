e9f02b070b8bf7ac48f98402b846a9b02315abec
PreethamSub <91767887+PreethamSub@users.noreply.github.com>
2023-09-21 12:12:45 +0530

    Refactor app.js

--- app.js
+++ app.js
@@ -13,27 +13,30 @@ const api_key = "Agv3yWf-P4aPelrHvZZfvISIRN2zWqCp885IYh2H_HnUpAjvzOhOXJRPiLUef4G
 
 app.get("/api/:lat/:lon/:zoom", async (req, res) => {
   //loading model
-  console.log("loading model");
+  console.log("Loading model");
   const model = await tf.loadLayersModel(model_url);
-  console.log("loaded model");
+  console.log("Loaded model");
 
   //downloading satellite image
   const { lat, lon, zoom } = req.params;
-  const imageUrl = `https://dev.virtualearth.net/REST/V1/Imagery/Map/Aerial/${lat}%2C${lon}/${zoom}?mapSize=224%2C224&format=jpeg&key=${api_key}`;
+  const imageUrl = `https://dev.virtualearth.net/REST/V1/Imagery/Map/Aerial/${lat}%2C${lon}/${zoom}?mapSize=500%2C550&format=jpeg&key=${api_key}`;
   const imagePth = "./pic.jpeg";
-
+  
+  console.log("Downloading satellite image:");
+  console.log(imageUrl);
   await client.get(imageUrl, (res) => {
     res.pipe(fs.createWriteStream(imagePth));
   });
+  await new Promise(resolve => setTimeout(resolve, 5000));
+  console.log("Downloaded satellite image");
 
   //predictions on the image
+  console.log("Reading satellite image");
   const buf = fs.readFileSync(imagePth);
   const input = tf.node.decodeJpeg(buf);
-  let imageTensor;
-  let pred;
-  imageTensor = tf.node.decodeJpeg(buf);
+  let imageTensor = tf.node.decodeJpeg(buf);
   imageTensor = imageTensor.expandDims(0);
-  console.log(`Success: local file to a ${imageTensor.shape} tensor`);
+  console.log(`Success: converted local file to a ${imageTensor.shape} tensor`);
   res.send({
     data: await model.predict(imageTensor, { batchSize: 4 }).data(),
   });
